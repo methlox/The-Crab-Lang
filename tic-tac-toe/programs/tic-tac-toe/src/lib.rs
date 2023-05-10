a428e9252dc74639664d5159528f0530db97285e
methlox <astroyellowh@gmail.com>
2023-05-10 23:20:10 +0530

    tests added\

--- tic-tac-toe/programs/tic-tac-toe/src/lib.rs
+++ tic-tac-toe/programs/tic-tac-toe/src/lib.rs
@@ -113,10 +113,23 @@ pub mod tic_tac_toe {
     // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
     //     Ok(())
     // }
-}
 
-pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
-    ctx.accounts.game.start([ctx.accounts.player_one.key(), player_two])
+    pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
+        ctx.accounts.game.start([ctx.accounts.player_one.key(), player_two])
+    }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        let game = &mut ctx.accounts.game;
+    
+        require_keys_eq!(
+            game.current_player(),
+            ctx.accounts.player.key(),
+            TicTacToeError::NotPlayersTurn
+        );
+    
+        game.play(&tile)
+    }
+    
 }
 
 
@@ -127,6 +140,13 @@ pub struct SetupGame<'info> {
     #[account(mut)]
     pub player_one: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Play<'info> {
+    #[account(mut)]
+    pub game: Account<'info, Game>,
+    pub player: Signer<'info>,
 }
 
 #[account]
