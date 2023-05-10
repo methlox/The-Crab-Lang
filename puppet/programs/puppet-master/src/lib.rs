b7455a35f1bad0b9a484b2c8b28c157aee67e398
methlox <astroyellowh@gmail.com>
2023-05-11 00:20:57 +0530

    cpi set up in puppet master

--- puppet/programs/puppet-master/src/lib.rs
+++ puppet/programs/puppet-master/src/lib.rs
@@ -1,3 +1,30 @@
+// use anchor_lang::prelude::*;
+// use puppet::cpi::accounts::SetData;
+// use puppet::program::Puppet;
+// use puppet::{self, Data};
+
+// declare_id!("HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L");
+
+// #[program]
+// mod puppet_master {
+//     use super::*;
+//     pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
+//         let cpi_program = ctx.accounts.puppet_program.to_account_info();
+//         let cpi_accounts = SetData {
+//             puppet: ctx.accounts.puppet.to_account_info(),
+//         };
+//         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+//         puppet::cpi::set_data(cpi_ctx, data)
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct PullStrings<'info> {
+//     #[account(mut)]
+//     pub puppet: Account<'info, Data>,
+//     pub puppet_program: Program<'info, Puppet>,
+// }
+
 use anchor_lang::prelude::*;
 use puppet::cpi::accounts::SetData;
 use puppet::program::Puppet;
@@ -9,12 +36,7 @@ declare_id!("HmbTLCmaGvZhKnn1Zfa1JVnp7vkMV4DYVxPLWBVoN65L");
 mod puppet_master {
     use super::*;
     pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
-        let cpi_program = ctx.accounts.puppet_program.to_account_info();
-        let cpi_accounts = SetData {
-            puppet: ctx.accounts.puppet.to_account_info(),
-        };
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        puppet::cpi::set_data(cpi_ctx, data)
+        puppet::cpi::set_data(ctx.accounts.set_data_ctx(), data)
     }
 }
 
@@ -23,4 +45,14 @@ pub struct PullStrings<'info> {
     #[account(mut)]
     pub puppet: Account<'info, Data>,
     pub puppet_program: Program<'info, Puppet>,
+}
+
+impl<'info> PullStrings<'info> {
+    pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetData<'info>> {
+        let cpi_program = self.puppet_program.to_account_info();
+        let cpi_accounts = SetData {
+            puppet: self.puppet.to_account_info()
+        };
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
 }
